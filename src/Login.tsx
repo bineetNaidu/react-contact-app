c24996d0cbd5d1810e2a0694b248b19185b0d86b
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-13 13:34:57 +0530

    Add Authenticatio w/ eamil && password.

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { projectAuth } from './firebase';
+import { useHistory } from 'react-router-dom';
 import './Login.css';
 
 const Login: React.FC = () => {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPass] = useState('');
+
+  const handleSignEmailAndPassword = async (
+    email: string,
+    password: string
+  ) => {
+    if (email && password) {
+      try {
+        const authUser = await projectAuth.signInWithEmailAndPassword(
+          email,
+          password
+        );
+        authUser && history.push('/');
+      } catch (e) {
+        alert(e.message);
+      }
+    }
+  };
+
   return (
-    <form className="login">
+    <form
+      className="login"
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSignEmailAndPassword(email, password);
+      }}
+    >
       <h1>Login</h1>
-      <TextField margin="normal" fullWidth label="Email" type="email" />
-      <TextField margin="normal" fullWidth label="Password" type="password" />
-      <Button variant="contained" color="primary">
+      <TextField
+        margin="normal"
+        fullWidth
+        label="Email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        margin="normal"
+        fullWidth
+        label="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPass(e.target.value)}
+      />
+      <Button variant="contained" color="primary" type="submit">
         Login
       </Button>
     </form>
