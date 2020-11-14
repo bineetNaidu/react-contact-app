f5b8852cbb998ca0d4ec5fa61aea6b9e703cd482
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-14 17:30:28 +0530

    - Add styles - updated Contact Component

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,7 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import useFirestore from './hooks/useFirestore';
 import { User } from './Types/User';
+import List from '@material-ui/core/List';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import ContactListItem from './ContactListItem';
+import './Contact.css';
 
 interface Props {
   user: User;
@@ -10,9 +15,32 @@ interface Props {
 
 const Contact: React.FC<Props> = ({ user, docId }) => {
   const { docs } = useFirestore(user.email!, docId);
+  const [expanded, setExpanded] = React.useState<string | false>(false);
 
+  const handleChange = (panel: string) => (
+    event: React.ChangeEvent<{}>,
+    isExpanded: boolean
+  ) => {
+    setExpanded(isExpanded ? panel : false);
+  };
   return (
-    <div>{docs && <Link to="/contacts/new">Create a new Contact</Link>}</div>
+    <div className="contact">
+      <Link to="/contacts/new" className="contact__floatingCtx">
+        <Fab color="primary" aria-label="Create a new Contact">
+          <AddIcon />
+        </Fab>
+      </Link>
+
+      <List dense className="contact__lists">
+        {docs.map((c) => (
+          <ContactListItem
+            {...c}
+            expanded={expanded}
+            handleChange={handleChange}
+          />
+        ))}
+      </List>
+    </div>
   );
 };
 
